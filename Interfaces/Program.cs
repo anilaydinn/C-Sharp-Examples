73a08b7c8d385259b582c4e61ec1ada159b38e1d
thracian22 <anil_aydinn@outlook.com>
2020-11-29 12:46:16 +0300

    Interfaces

---
+++ Interfaces/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PersonManager personManager = new PersonManager();
+            personManager.Add(new Customer {Id=1, FirstName="Anıl", LastName="Aydın", Address="Edirne" });
+
+            Console.ReadLine();
+        }
+    }
+
+    interface IPerson
+    {
+       int Id { get; set; }
+       string FirstName { get; set; }
+       string LastName { get; set; }
+    }
+
+    class Customer:IPerson
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+    }
+
+    class Student:IPerson
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Department { get; set; }
+    }
+
+    class PersonManager
+    {
+        public void Add(IPerson person)
+        {
+            Console.WriteLine(person.FirstName);
+        }
+    }
+}
