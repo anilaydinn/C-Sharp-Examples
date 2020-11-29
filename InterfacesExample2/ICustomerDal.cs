c884a10e05b3ae4d3daa6742820364629329fd30
thracian22 <anil_aydinn@outlook.com>
2020-11-29 13:37:23 +0300

    InterfacesExample2

---
+++ InterfacesExample2/ICustomerDal.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfacesExample2
+{
+    interface ICustomerDal
+    {
+        void Add();
+        void Update();
+        void Delete();
+    }
+
+    class SqlServerCustomerDal:ICustomerDal
+    {
+        public void Add()
+        {
+            Console.WriteLine("Sql added");
+        }
+
+        public void Update()
+        {
+            Console.WriteLine("Sql updated");
+        }
+
+        public void Delete()
+        {
+            Console.WriteLine("Sql deleted");
+        }
+    }
+
+    class OracleCustomerDal:ICustomerDal
+    {
+        public void Add()
+        {
+            Console.WriteLine("Oracle added");
+        }
+
+        public void Update()
+        {
+            Console.WriteLine("Oracle updated");
+        }
+
+        public void Delete()
+        {
+            Console.WriteLine("Oracle deleted");
+        }
+    }
+
+    class MysqlCustomerDal : ICustomerDal
+    {
+        public void Add()
+        {
+            Console.WriteLine("Mysql added");
+        }
+
+        public void Update()
+        {
+            Console.WriteLine("Mysql updated");
+        }
+
+        public void Delete()
+        {
+            Console.WriteLine("Mysql deleted");
+        }
+    }
+}
---
+++ InterfacesExample2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfacesExample2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ICustomerDal[] customerDals = new ICustomerDal[3] { new SqlServerCustomerDal(), new OracleCustomerDal(), new MysqlCustomerDal() };
+
+            foreach(var customerDal in customerDals)
+            {
+                customerDal.Add();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
