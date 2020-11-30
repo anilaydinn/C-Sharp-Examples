148e04514447e55fda90240d6ab880b490bb5524
thracian22 <anil_aydinn@outlook.com>
2020-12-01 00:31:38 +0300

    Collections3

---
+++ Collections3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collections3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> dictionary = new Dictionary<string, string>();
+            dictionary.Add("book", "kitap");
+            dictionary.Add("table", "masa");
+            dictionary.Add("computer", "bilgisayar");
+
+            Console.WriteLine(dictionary["table"]);
+
+            foreach (var item in dictionary)
+            {
+                Console.WriteLine(item.Key);
+            }
+
+            Console.WriteLine(dictionary.ContainsKey("glass"));
+            Console.WriteLine(dictionary.ContainsKey("table"));
+
+            Console.ReadLine();
+        }
+    }
+}
