fe036eed17f1f638bc61920da43530ad8870ebf3
thracian22 <anil_aydinn@outlook.com>
2020-12-01 22:21:45 +0300

    Exceptions

---
+++ Exceptions/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exceptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string[] students = new string[3] { "Engin", "Derin", "Salih" };
+                students[3] = "Ahmet";
+            }
+            catch(IndexOutOfRangeException exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
